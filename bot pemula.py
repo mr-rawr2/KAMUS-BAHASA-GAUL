cbab0fdc83fbe302954de08efa3002528b8056b1
mr-rawr2 <154895802+mr-rawr2@users.noreply.github.com>
2024-01-16 20:41:09 +0700

    Create bot pemula.py

---
+++ bot pemula.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+from projec import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx):
+    await ctx.send('berikut password untukmu')
+    await ctx.send(gen_pass())
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+@bot.command()
+async def meme_random(ctx):
+    """bagi meme indo"""
+    await ctx.send('apa memenya?')
